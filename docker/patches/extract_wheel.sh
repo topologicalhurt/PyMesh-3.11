3a647fa0399a37887803b69bc493afc77ef27f20
Daniel Sali <salid@ccm.rw5it4hghpkudjhai3vhym4npa.ax.internal.cloudapp.net>
2023-08-17 16:10:58 +0000

    Build py310 version 1

--- docker/patches/extract_wheel.sh
+++ docker/patches/extract_wheel.sh
@@ -5,7 +5,7 @@ export PYMESH_IMAGE="pymesh/pymesh"
 
 mkdir -p $DIST_DIR
 
-for TAG in "py2.7" "py3.5" "py3.6" "py3.7"
+for TAG in "py2.7" "py3.5" "py3.6" "py3.7 py3.10"
 do
     docker pull $PYTHON_IMAGE:$TAG
     docker run --rm -v `pwd`/$DIST_DIR:/mnt $PYMESH_IMAGE:$TAG bash -c "cp /root/PyMesh/dist/*.whl /mnt/; cp /root/PyMesh/python/requirements.txt /mnt/"
