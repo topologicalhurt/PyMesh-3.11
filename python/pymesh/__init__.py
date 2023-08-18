bb2217000a867230c73cadd142a61d8a3d2d9c42
Daniel Sali <salid@ccm.rw5it4hghpkudjhai3vhym4npa.ax.internal.cloudapp.net>
2023-08-18 05:08:45 +0000

    Remove deprecated numpy.testing.Tester import

--- python/pymesh/__init__.py
+++ python/pymesh/__init__.py
@@ -2,8 +2,6 @@ from .version import __version__
 from . import PyMeshSetting
 from .timethis import timethis
 
-from numpy.testing import Tester
-test = Tester().test
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
