cdb5937f27a3f1d4d115ec73a425cf044076bea5
topologicalhurt <csin0659@uni.sydney.edu.au>
2023-10-10 19:31:37 +1100

    Added python 3.11.6 to docker build

--- docker/patches/extract_wheel.sh
+++ docker/patches/extract_wheel.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env bash
-
-export DIST_DIR="dist"
-export PYMESH_IMAGE="pymesh/pymesh"
-
-mkdir -p $DIST_DIR
-
-for TAG in "py2.7" "py3.5" "py3.6" "py3.7 py3.10"
-do
-    docker pull $PYTHON_IMAGE:$TAG
-    docker run --rm -v `pwd`/$DIST_DIR:/mnt $PYMESH_IMAGE:$TAG bash -c "cp /root/PyMesh/dist/*.whl /mnt/; cp /root/PyMesh/python/requirements.txt /mnt/"
-done
+#!/usr/bin/env bashexport DIST_DIR="dist"export PYMESH_IMAGE="pymesh/pymesh"mkdir -p $DIST_DIR# shellcheck disable=SC2043for TAG in "py 3.11.6"do    docker pull "$PYTHON_IMAGE":"$TAG"    # shellcheck disable=SC2046    # shellcheck disable=SC2006    docker run --rm -v `pwd`/$DIST_DIR:/mnt $PYMESH_IMAGE:"$TAG" bash -c "cp /root/PyMesh/dist/*.whl /mnt/; cp /root/PyMesh/python/requirements.txt /mnt/"done#for TAG in "py2.7" "py3.5" "py3.6" "py3.7 py3.10"#do#    docker pull $PYTHON_IMAGE:$TAG#    docker run --rm -v `pwd`/$DIST_DIR:/mnt $PYMESH_IMAGE:$TAG bash -c "cp /root/PyMesh/dist/*.whl /mnt/; cp /root/PyMesh/python/requirements.txt /mnt/"#done
